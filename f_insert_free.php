acb2378938fc6cbd719f3c1b8be12d070f0927c8
Andre Nitschke <info@senft-betonbohren.de>
2014-10-16 10:23:59 +0200

    Create f_insert_free.php

---
+++ f_insert_free.php
@@ -0,0 +1,24 @@
+<!-- Bedaplan2 Version 2.0.0 16.10.2014 https://github.com/SenftGmbH/ -->
+<?php
+
+$mitarbeiter = $_GET["current_user"];
+$current_nr = $_GET["current_nr"];
+	
+?>
+
+<html>
+ <head>
+  <title>bedaplan</title>
+  <meta name="viewport" content="width=500" />
+ </head>
+ <body>
+ <img src="http://213.23.35.50:8688/bedaplan/bedaplanlogo.jpg">
+Freier Tag Mitarbeiter <?php echo $mitarbeiter ?>
+<br><br>
+<center>
+ <a href="http://213.23.35.50:8688/bedaplan/insert_free.php?current_user=<?php echo $mitarbeiter ?>"  target="_blank"><img src="frei.jpg" border=0></a><br><br><br>
+
+ <a href="javascript:window.close()"><img src="hauptseite.jpg" border=0></a>
+</center>  
+ </body>
+</html>
